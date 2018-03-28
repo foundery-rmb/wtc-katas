b579b294e9e1f1c0613f8a9831c665b37801f1c7
Theo Bohnen <tbohnen.jnr@gmail.com>
2018-03-28 11:15:39 +0200

    Finished Fizz Buzz in Java

--- FizzBuzz/javascript/fizz-buzz.js
+++ FizzBuzz/javascript/fizz-buzz.js
@@ -1,5 +1,6 @@
 module.exports = function(intToConvert){
-    if (!parseInt(intToConvert)){
-        throw "Invalid input.";
-    }
+    if (intToConvert % 3 === 0 && intToConvert % 5 === 0) return "FizzBuzz"
+    else if (intToConvert % 3 === 0) return "Fizz"
+    else if (intToConvert % 5 === 0) return "Buzz"
+    else return intToConvert.toString();
 }
--- FizzBuzz/javascript/test/fizz-buzz-tests.js
+++ FizzBuzz/javascript/test/fizz-buzz-tests.js
@@ -6,7 +6,9 @@ var fizzBuzz = require('../fizz-buzz');
 //of five print “Buzz”.
  //For numbers which are multiples of both three and five print “FizzBuzz”.
 
+
 describe('FizzBuzz Kata', function(){
+
     it('Should not throw an error if it is a number',function(){
         fizzBuzz(100);
     });
@@ -15,7 +17,12 @@ describe('FizzBuzz Kata', function(){
         var errorThrown = false
 
         try {
-            fizzBuzz("blah");
+            var { error } = fizzBuzz("blah");
+
+            if (error){
+
+            }
+
         }
         catch (e){
             if (e === "Invalid input."){
@@ -25,7 +32,28 @@ describe('FizzBuzz Kata', function(){
 
         expect(errorThrown).to.be.true;
     });
+
+    it("if it's not divisable by three or five, return the same number", function(){
+        const result = fizzBuzz(4);
+
+        expect(result).to.eql("4");
+    });
+
+    it("should return Fizz if divisable by three", function(){
+        const result = fizzBuzz(3);
+
+        expect("Fizz").to.eql(result);
+    });
+
+    it("should return Buzz if divisable by five", function(){
+        const result = fizzBuzz(5);
+
+        expect("Buzz").to.eql(result);
+    });
+    
+    it("should return FizzBuzz if divisable by three and five", function(){
+        const result = fizzBuzz(15);
+
+        expect("FizzBuzz").to.eql(result);
+    });
 });
-
-
-
